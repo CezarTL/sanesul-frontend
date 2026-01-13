c493283fec7396517de17127b6af47cf7c709909
CezarTL <cezar.icarus@gmail.com>
2026-01-13 08:48:05 -0400

    Create App.jsx

---
+++ src/📄 src/App.jsx
@@ -0,0 +1,13 @@
+import Login from './pages/Login'
+import AnalisarFoto from './pages/AnalisarFoto'
+
+export default function App() {
+  return (
+    <div>
+      <h1>Sanesul IA</h1>
+      <Login />
+      <hr />
+      <AnalisarFoto />
+    </div>
+  )
+}
